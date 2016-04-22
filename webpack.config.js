44d53326cfcd3dd27dc53ec1c1e28cae587b3f08
Ivan Santos <pragmaticivan@gmail.com>
2016-04-22 00:12:42 -0700

    partial webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,59 @@
-module.exports = {
-  entry: "./web/static/js/app.js",
+"use strict";
+
+var path = require("path");
+
+var ExtractTextPlugin = require("extract-text-webpack-plugin"),
+    webpack = require("webpack");
+
+// helpers for writing path names
+// e.g. join("web/static") => "/full/disk/path/to/hello/web/static"
+function join(dest) { return path.resolve(__dirname, dest); }
+function web(dest) { return join("web/static/" + dest); }
+
+var config = module.exports = {
+  // our app's entry points - for this example we'll use a single each for
+  // css and js
+  entry: [
+    web("css/app.scss"),
+    web("js/app.js")
+  ],
+
+  // where webpack should output our files
   output: {
-    path: "./priv/static/js",
-    filename: "app.js"
-  }
+    path: join("priv/static"),
+    filename: "js/app.js"
+  },
+
+  // more information on how our modules are structured, and
+  //
+  // in this case, we'll define our loaders for JavaScript and CSS.
+  // we use regexes to tell Webpack what files require special treatment, and
+  // what patterns to exclude.
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel"
+      }, {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract("style", "css!sass")
+      }
+    ]
+  },
+
+  // what plugins we'll be using - in this case, just our ExtractTextPlugin.
+  // we'll also tell the plugin where the final CSS file should be generated
+  // (relative to config.output.path)
+  plugins: [
+    new ExtractTextPlugin("css/app.css")
+  ]
 };
+
+// if running webpack in production mode, minify files with uglifyjs
+if (process.env.NODE_ENV === "production") {
+  config.plugins.push(
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({ minimize: true })
+  );
+}
